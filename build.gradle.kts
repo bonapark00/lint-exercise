ffad63848c7a04de8b76bd624ad1bb5d712dc798
Jaeyeon(Bona) Park <jaeyeon.park@hcs.snu.ac.kr>
2025-08-17 13:55:28 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,16 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1" // ktlint 플러그인
-
 }
 
 ktlint {
     android.set(true)               // Android 규칙 활성화
     ignoreFailures.set(false)       // 위반 시 빌드 실패
+    filter {
+        include("**/*.kt")
+        exclude("**/*.kts") // Gradle Kotlin DSL 파일 제외 (임시용)
+    }
     reporters {
         reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
         reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
