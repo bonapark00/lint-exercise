dbc3515161d9fc278ca2c98067acde5c09298c18
Jaeyeon(Bona) Park <jaeyeon.park@hcs.snu.ac.kr>
2025-08-17 14:45:48 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,24 +9,23 @@ plugins {
 
 // 모든 하위 모듈(app 등)에 ktlint 적용 + 공통 설정
 subprojects {
-    plugins.apply("org.jlleitschuh.gradle.ktlint")
+    plugins.withId("org.jlleitschuh.gradle.ktlint") {
+        afterEvaluate {
+            extensions.configure<org.jlleitschuh.gradle.ktlint.KtlintExtension>("ktlint") {
+                android.set(true)
+                ignoreFailures.set(false)
 
-    extensions.configure<org.jlleitschuh.gradle.ktlint.KtlintExtension>("ktlint") {
-        android.set(true)
-        ignoreFailures.set(false)
+                reporters {
+                    reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+                    reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+                }
 
-        // 보고서 포맷
-        reporters {
-            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
-            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
-        }
-
-        // 검사 대상/제외 경로 (대소문자 주의: androidTest)
-        filter {
-            include("**/*.kt")
-            exclude("**/*.kts")            // Gradle Kotlin DSL은 제외(원하면 지우세요)
-            exclude("**/src/test/**")
-            exclude("**/src/androidTest/**")
+                filter {
+                    include("**/*.kt")
+                    exclude("**/src/test/**")
+                    exclude("**/src/androidTest/**") // <-- 실제로 제외되도록 보장
+                }
+            }
         }
     }
 }
